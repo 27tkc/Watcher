becd2cb4a6b9701968898dfff3551185bf23aeec
Tarun Kumar Choudhary <tarunkc22@gmail.com>
2024-02-10 07:59:04 -0500

    Front End

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+import axios from "axios";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get("/videos/random");
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, []);
+
+  return (
+    <Container>
+      {videos.map((video) => (
+        <Card />
+      ))}
+    </Container>
+  );
+};
+
+export default Home;
