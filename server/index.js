55b154c9ff7cb84221c937fac783d4269844130f
Tarun Kumar Choudhary <tarunkc22@gmail.com>
2024-04-18 14:59:21 -0400

    fix4

--- server/index.js
+++ server/index.js
@@ -8,44 +8,30 @@ import authRoutes from "./routes/auth.js";
 import paymentRoutes from "./routes/payment.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-const app = express();
-dotenv.config();
-app.use(cors());
-const connect = () => {
-  mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-      console.log("Connected to DB");
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
 
-//middlewares
+dotenv.config();
+
+const app = express();
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-// app.use("/", (req, res) => {
-//   res.send("Server is running");
-// });
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/payments", paymentRoutes);
 
-//error handler
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong!";
-  return res.status(status).json({
-    success: false,
-    status,
-    message,
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start the server after successfully connecting to the database
+    app.listen(2700, () => {
+      console.log("Server is running on port 2700");
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
   });
-});
-
-app.listen(2700, () => {
-  connect();
-  console.log("Connected to Server");
-});
