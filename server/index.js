db080d335a99ff81697ad4b65fcc9cbb878b2d57
Tarun Kumar Choudhary <tarunkc22@gmail.com>
2024-04-18 14:44:14 -0400

    Vercel fix

--- server/index.js
+++ server/index.js
@@ -25,9 +25,9 @@ const connect = () => {
 //middlewares
 app.use(cookieParser());
 app.use(express.json());
-app.use("/", (req, res) => {
-  res.send("Server is running");
-});
+// app.use("/", (req, res) => {
+//   res.send("Server is running");
+// });
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
